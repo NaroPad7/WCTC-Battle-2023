e454481c0310ef014b96e5fe860cae2fcb56907f
NaroPad7 <120739676+NaroPad7@users.noreply.github.com>
2023-09-22 16:55:44 -0500

    added implementation of the interface

--- Program.cs
+++ Program.cs
@@ -8,13 +8,23 @@ namespace Class05
         static void Main(string[] args)
         {
 
+
             IArmor armor = new Leather("Elven Leather Armor", 12);
             IWeapon sword = new Sword("Long sword", 5);
+            // new armor and new weapon
 
+            IArmor newArmor = new Leather("Space Armor", 25);
+            IWeapon newWeapon = new Sword("Light Saber", 100);
+
+            
             ICombatant fighter = new Fighter("Fighter Fred", armor, sword);
+            // newCombatant
             ICombatant badGuy = new Fighter("Scary Monster", armor, sword);
 
-            fighter.Attack(badGuy);
+            ICombatant jedi = new Fighter("Luke Skywalker", newArmor, newWeapon);
+
+            jedi.Attack(badGuy);
+
 
         }
 
